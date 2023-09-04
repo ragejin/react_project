5bba599bc92900a059356765e608bd8ae84ba98e
Ruijie Jin <ruijiejin@JRJ-2.home>
2023-09-03 21:33:51 -0700

    frist commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+
+function Navbars() {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbars
